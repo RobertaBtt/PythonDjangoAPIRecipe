ba6762a5970bf3f564467ab71f95e41088c56cb3
RobertaBtt <roberta.btt@gmail.com>
2021-04-14 12:24:17 +0200

    Added manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,7 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         """Update a user, setting the password correctly and return it"""
-        password = validated_data.pop('password', None)
+        password = validated_data.pop('password', None)  # None is the default value
         user = super().update(instance, validated_data)
 
         if password:
