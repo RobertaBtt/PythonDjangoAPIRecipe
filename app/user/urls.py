4ac1dcf381da582e208b8a55dafde2edd8e8d738
RobertaBtt <roberta.btt@gmail.com>
2022-11-03 09:26:16 +0100

    Updated tests and url and views of recipe and user

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,3 +1,6 @@
+"""
+URL mappings for the user API.
+"""
 from django.urls import path
 
 from user import views
@@ -9,5 +12,4 @@ urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
     path('me/', views.ManageUserView.as_view(), name='me'),
-
 ]
